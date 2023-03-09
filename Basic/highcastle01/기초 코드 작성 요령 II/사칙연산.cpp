6624e23ad86b4f70e32b87fcc8c438362a4bcf89
highcastle01 <76648806+highcastle01@users.noreply.github.com>
2023-03-09 14:00:06 +0900

    Create 사칙연산.cpp

---
+++ Basic/highcastle01/기초 코드 작성 요령 II/사칙연산.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int a,b;
+	
+	cin >> a>>b;
+	
+	cout << a+b << endl << a-b << endl << a*b << endl << a/b << endl << a%b;
+}
