2301124274236560a197f1f7fc7a693a6d715e99
youth5427 <127531421+youth5427@users.noreply.github.com>
2023-03-11 16:17:20 +0900

    Add files via upload

---
+++ Basic/youth5427/basic_code/beak_10171.cpp
@@ -0,0 +1,14 @@
+/*
+	Day: 2023.03.10
+	참고 : 역슬래쉬(\) 출력
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "\\    /\\" << endl;
+	cout << " )  ( \')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|";
+}
