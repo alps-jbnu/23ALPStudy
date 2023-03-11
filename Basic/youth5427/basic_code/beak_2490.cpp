54a08193cc85494f4c0e2770e622cc8872f7f76b
youth5427 <127531421+youth5427@users.noreply.github.com>
2023-03-11 16:50:31 +0900

    Add files via upload

---
+++ Basic/youth5427/basic_code/beak_2490.cpp
@@ -0,0 +1,22 @@
+/*
+	Day: 2023.03.11
+	Âü°í :
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c, d;
+	for (int i = 0; i < 3; i++) {
+		int count = 0;
+		cin >> a >> b >> c >> d;
+		if (a == 1)	count++;	if (b == 1)count++;
+		if (c == 1)count++;		if (d == 1)count++;
+
+		if (count == 1) cout << "C" << endl;
+		else if (count == 2) cout << "B" << endl;
+		else if (count == 3) cout << "A" << endl;
+		else if (count == 4) cout << "E" << endl;
+		else if (count == 0) cout << "D" << endl;
+	}
+}
