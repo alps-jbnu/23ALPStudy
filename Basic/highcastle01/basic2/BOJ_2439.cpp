663cda66bbeca37d36c98fc0f01d00584064ddf8
highcastle01 <76648806+highcastle01@users.noreply.github.com>
2023-03-11 16:17:39 +0900

    Create BOJ_2439.cpp

---
+++ Basic/highcastle01/basic2/BOJ_2439.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n, i, j, k;
+	cin >> n;
+	
+	for(i = 1; i <= n; i++)
+	{
+		for(j = 1; j <= n - i; j++)
+		{
+			cout << " ";
+		}
+		for(k = 1; k <= i; k++)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+	
+	return 0;
+}
