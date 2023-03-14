d2ed69cb89afddbb9d5b43380c12672d96781234
junest66 <chlwndks333@gmail.com>
2023-03-14 22:40:10 +0900

    Add 18870.cpp

---
+++ Intermediate/chlwndks33/Binary Search/18870.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    vector<int> vec;
+    vector<int> cp;
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        int a;
+        cin >> a;
+        vec.push_back(a);
+    }
+    cp = vec;
+    sort(vec.begin(),vec.end());
+    vec.erase(unique(vec.begin(),vec.end()),vec.end());
+    for(int i=0; i<n; i++) {
+        cout << upper_bound(vec.begin(),vec.end(),cp[i]) - vec.begin()-1 << " ";
+    }
+
+}
