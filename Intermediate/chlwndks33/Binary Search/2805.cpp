625d2ff06145911987bf0ef537495fab77b0584a
junest66 <chlwndks333@gmail.com>
2023-03-15 19:16:54 +0900

    Add 2805.cpp

---
+++ Intermediate/chlwndks33/Binary Search/2805.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int ans;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n,m,height,a;
+    vector<int> vec;
+    cin >> n >> m;
+    for(int i=0; i<n; i++) {
+        cin >> a;
+        vec.push_back(a);
+    }
+    int st = 0;
+    int en = *max_element(vec.begin(),vec.end());
+    while(st <= en) {
+        int mid = (st + en) / 2; //나무의 높이
+        long long sum = 0;
+        for(int i=0; i<n; i++) {
+            if(vec[i]>mid) sum += vec[i] - mid;
+        }
+        if(sum >= m) { //집에 돌아갈 수 있는 경우
+            ans = mid;
+            st = mid + 1;
+        }
+        else { //집에 못 가는 경우
+            en = mid - 1;
+        }
+    }
+    cout << ans;
+
+}
