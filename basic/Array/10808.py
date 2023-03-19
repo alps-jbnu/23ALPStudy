a82a3b6b1008beb311546a383adea500cbda48be
LeeJungHyun <55616895+AFpine@users.noreply.github.com>
2023-03-19 20:49:53 +0900

    Revert "Array"

--- basic/Array/10808.py
+++
@@ -1,7 +0,0 @@
-s = input()
-alpha = "abcdefghijklmnopqrstuvwxyz"
-for i in range(len(alpha)):
-    if s.count(alpha[i]):
-        print(s.count(alpha[i]), end=' ')
-    else:
-        print('0', end=' ')
