190f51a2433106f56e364b5c8fcc4e6db2e8a55b
rladntlr <tlrrns08@gmail.com>
2023-03-24 16:43:47 +0900

    Fix: 이름 변경 및 잔여 코드 추가

---
+++ Basic/eddiego/1.기초 코드 작성 요령 2/15552.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    for(int t = 0; t < T; t++){
+        int A, B;
+        cin >> A >> B;
+
+        cout << A + B << "\n";
+    }
+}
---
+++ Basic/eddiego/1.기초 코드 작성 요령 2/2440.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N - i; j++)  cout << '*';
+        cout << "\n";
+    }
+}
