87de3c6b097d61777497b7bf170f8a7c9979067f
jhyun13 <122020140+jhyun13@users.noreply.github.com>
2023-03-28 19:14:09 +0900

    Update 11055.cpp

--- Intermediate/kimmi7574/DP/11055.cpp
+++ Intermediate/kimmi7574/DP/11055.cpp
@@ -14,18 +14,7 @@ int main() {
 	for (int i = 1; i <= n; i++) {
 		cin >> a[i];
 	}
-
-	//�ʱⰪ
-	/*d[1] = a[1];
-	max_sum = d[1];
-
-	for (int i = 2; i <= n; i++) {
-		if ((a[i] - a[i - 1]) > 0) {
-			d[i] = d[i - 1] + a[i];
-			max_sum = max(max_sum, d[i]);
-		}
-	}*/
-
+	
 	for (int i = 1; i <= n; i++) {
 		d[i] = a[i];
 		for (int j = 1; j < i; j++) {
