6854ff7c1038e08dc421546cd43e6e48a19c2f11
Jeon Young Sang <106549866+Imvideo@users.noreply.github.com>
2023-03-29 14:47:20 +0900

    Add files via upload

---
+++ Basic/iwtl0303/Stack/10828.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  deque<int> DQ;
+  int N,M,num,cnt=0;
+
+  cin>>N>>M;
+  for (int i=1;i<=N;i++){
+    DQ.push_back(i);
+  }
+  for(int i=0;i<M;i++){
+    cin>>num;
+    int idx = find(DQ.begin(),DQ.end(),num) - DQ.begin();  //find 함수의 존재를 몰랐음
+                                                           //DQ.begin()을 빼는 이유는 find의 반환형은 iterator인데 int로 바꾸기 위함으로 추정
+    while(num != DQ.front()) {
+      if (idx < DQ.size()-idx){
+        DQ.push_back(DQ.front());
+        DQ.pop_front();
+      } 
+      else {
+        DQ.push_front(DQ.back());
+        DQ.pop_back();
+      }
+      cnt++;
+    }
+    DQ.pop_front();
+  }
+  cout << cnt;
+}
