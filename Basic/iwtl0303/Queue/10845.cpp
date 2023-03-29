3132652b95e9aee7fd6665f8f5ed2cf97809f891
Jeon Young Sang <106549866+Imvideo@users.noreply.github.com>
2023-03-29 17:39:36 +0900

    Update 10845.cpp

--- Basic/iwtl0303/Queue/10845.cpp
+++ Basic/iwtl0303/Queue/10845.cpp
@@ -15,7 +15,7 @@ int main(void)
   }
   for(int i=0;i<M;i++){
     cin>>num;
-    int idx = find(DQ.begin(),DQ.end(),num) - DQ.begin();  //find 함수의 존재를 몰랐음
+    int idx = find(DQ.begin(),DQ.end(),num) - DQ.begin();  //함수의 존재를 몰랐음
                                                            //DQ.begin()을 빼는 이유는 find의 반환형은 iterator인데 int로 바꾸기 위함으로 추정
     while(num != DQ.front()) {
       if (idx < DQ.size()-idx){
