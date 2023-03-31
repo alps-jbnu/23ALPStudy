ce71eb032e67b51feb10dafd21e468c41aac4684
junest66 <chlwndks333@gmail.com>
2023-03-31 20:52:55 +0900

    Add 1931.cpp

---
+++ Intermediate/chlwndks33/Greedy/1931.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <utility>
+using namespace std;
+
+int n;
+pair<int,int> s[100001];  //{end time, st time}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for(int i=0; i<n; i++)
+        cin >> s[i].second >> s[i].first;
+    sort(s,s+n);
+
+    int ans = 0;
+    int t = 0;
+    for(int i=0; i<n; i++) {
+        if(t > s[i].second) continue;
+        ans++;
+        t = s[i].first;
+    }
+    cout << ans;
+}
