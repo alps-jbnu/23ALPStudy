500bb62953ee0f979a2dc3c18fb5dc0887d96afd
dameun2224 <113423804+dameun2224@users.noreply.github.com>
2023-05-25 02:52:22 +0900

    Create 11441.cpp

---
+++ Intermediate/Dameun2224/Prefix_Sum/11441.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int num[100005];
+int sum[100005];
+
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+		cin >> num[i];
+
+	for (int i = 1; i <= n; i++)
+		sum[i] = sum[i - 1] + num[i - 1];
+	
+	cin >> m;
+	while (m--) {
+		int a, b;
+		cin >> a >> b;
+		cout << sum[b] - sum[a - 1] << '\n';
+	}
+
+	return 0;
+}
