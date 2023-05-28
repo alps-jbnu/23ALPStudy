acc3732a9f84d8b56172b9f1a838810231f4b730
highcastle01 <76648806+highcastle01@users.noreply.github.com>
2023-05-28 22:42:56 +0900

    Create BOJ_11279.cpp

---
+++ Basic/highcastle01/Heap/BOJ_11279.cpp
@@ -0,0 +1,118 @@
+#include<iostream>
+
+using namespace std;
+
+#define MAX_ELEMENT 100001
+
+class HeapNode
+{
+	int key;
+	public:
+		HeapNode(int k = 0) : key(k){}
+		void setKey(int k){key = k;}
+		int getKey(){return key;}
+		void display(){cout << key << "\n";}
+};
+
+class MaxHeap : public HeapNode
+{
+	HeapNode node[MAX_ELEMENT];
+	int size;
+public:
+	MaxHeap() : size(0){}
+	bool isEmpty(){return size == 0;}
+	bool isFull(){return size == MAX_ELEMENT -1;}
+	HeapNode& getParent(int i){return node[i/2];}
+	HeapNode& getLeft(int i){return node[i*2];}
+	HeapNode& getRight(int i){return node[i*2+1];}
+	int getSize(){return size;}
+	
+	void display()
+    {
+        node[1].display();
+    }
+	
+	void insert(int key)
+	{
+		if(isFull())
+		{
+			return;
+		}
+		
+		int i = ++size;
+		while(i != 1 && key > getParent(i).getKey())
+		{
+			node[i] = getParent(i);
+			i /= 2;
+		}
+		
+		node[i].setKey(key);
+	}
+	
+	HeapNode remove()
+	{
+		if(isEmpty())
+		{
+			return NULL;
+		}
+		HeapNode root = node[1];
+		HeapNode last = node[size--];
+		
+		int parent = 1;
+		int child = 2;
+		
+		while(child <= size)
+		{
+			if(child < size && getLeft(parent).getKey() < getRight(parent).getKey())
+			{
+				child++;
+			}
+			if(last.getKey() >= node[child].getKey())
+			{
+				break;
+			}
+			
+			node[parent] = node[child];
+			parent = child;
+			child *= 2;
+		}
+		
+		node[parent] = last;
+		
+		return root;
+	}
+};
+
+int main(void)
+{	
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n, k, i, x;
+	MaxHeap max;
+	
+	cin >> n;
+	
+	for(i = 1; i <= n; i++)
+	{
+		cin >> x;
+		
+		if(x == 0)
+		{
+			if(max.isEmpty())
+			{
+				cout << "0\n";
+			}
+			else
+			{
+				max.display();
+				max.remove();
+			}
+		}
+		else if(x > 0)
+		{
+			max.insert(x);
+		}
+	}
+		
+	return 0;
+}
