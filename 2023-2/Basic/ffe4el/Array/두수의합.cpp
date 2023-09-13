0ebbe3d820742a347d25c790a1a9cba8ecc0691d
ffe4el <codkan20@gmail.com>
2023-09-14 01:39:11 +0900

    두수의합_최적화 필요

--- 2023-2/Basic/ffe4el/Array/두수의합.cpp
+++ 2023-2/Basic/ffe4el/Array/두수의합.cpp
@@ -2,16 +2,38 @@
 #include <vector>
 using namespace std;
 
+//최적화가 관건인듯...
 int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     int n;
     cin >>n;
 
     int a;
-    vector<int> arr;
+    vector<int> v;
     for(int i=0; i<n; i++){
         cin >> a;
-        arr.push_back(a); //입력받은 숫자, 배열에 저장
+        v.push_back(a); //입력받은 숫자, 배열에 저장
     }
+
+    int m;
+    cin >>m;
+
+    int cnt=0;
+    for(int i=0; i<n; i++){
+        int b = v.at(i);
+        cout<< "b" << b << endl;
+        for(int j=0; j<n; j++){
+            int c= v.at(j);
+            cout<< "c" << c << endl;
+            if(b+c==m){
+                cnt++;
+            }
+        }
+    }
+
+    cout << cnt;
 
     return 0;
 }
