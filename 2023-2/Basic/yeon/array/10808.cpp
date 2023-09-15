eca94c210aa4e0b3233da40dcb0800e295f9ccf5
juyeon777 <130140851+juyeon777@users.noreply.github.com>
2023-09-15 18:37:28 +0900

    Update 10808.cpp

--- 2023-2/Basic/yeon/array/10808.cpp
+++ 2023-2/Basic/yeon/array/10808.cpp
@@ -32,3 +32,25 @@ int main(){
     for(int i=0; i<26; i++) cout << freq[i] << ' ';
 }
 */
+
+
+/////////////
+
+/*정답 코드
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int freq[26];
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  string s;
+  cin >> s;
+  for(auto c : s)
+    freq[c-'a']++;
+  for(int i = 0; i < 26; i++)
+    cout << freq[i] << ' ';
+}
+
+*/
