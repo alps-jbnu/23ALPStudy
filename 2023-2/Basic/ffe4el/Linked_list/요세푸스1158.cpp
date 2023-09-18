8abeb56502dc2c4e80c4756c0a9e11a554975979
ffe4el <codkan20@gmail.com>
2023-09-19 01:04:58 +0900

    요세푸스 큐 풀이완료

---
+++ 2023-2/Basic/ffe4el/Linked_list/요세푸스1158.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+//큐를 이용해서 풀기
+int main(){
+    queue <int> q;
+    int n,k;
+    string ans;
+    cin >> n;
+    cin >> k;
+    for(int i=1; i<=n; i++){
+        q.push(i);
+    }
+
+    cout <<"<";
+    for(int j=0; j<n-1; j++){
+        for(int i=0; i<k-1; i++){
+            q.push(q.front());
+            q.pop();
+        }
+        cout << q.front() << ", ";
+        q.pop();
+    }
+    cout << q.front() <<">"<<endl;
+    return 0;
+}
+
+
+//배열을 써서 해보려고 했는데, 시간복잡도가 너무 큼... n제곱이상임...
+// int main(){
+//     int arr[5001];
+//     int n,k;
+//     cin >> n;
+//     cin >> k;
+//     for(int i=1; i<=n; i++){ //배열에 n까지의 수 넣기 
+//         arr[i] = i;
+//     }
+//     return 0;
+// }
