70754c2d25a79026ffcf90bb5b7a6bc78487c9f8
juyeon777 <130140851+juyeon777@users.noreply.github.com>
2023-09-20 22:21:51 +0900

    Update 1874.cpp

--- 2023-2/Basic/yeon/stack/1874.cpp
+++ 2023-2/Basic/yeon/stack/1874.cpp
@@ -6,17 +6,13 @@ using namespace std;
 
 int main()
 {
-    
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);    
-    
+  
     int t;
     cin >> t;
     
     vector <char> v;
     stack <int> s;
-    
+
     int k = 1; //입력값과 비교
     
     for(int i=0; i<t; i++){
