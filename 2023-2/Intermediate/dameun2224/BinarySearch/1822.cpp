88ffe11b29b21c3fcd4ce6a3158887c5946b97f0
dameun2224 <113423804+dameun2224@users.noreply.github.com>
2023-09-21 00:08:55 +0900

    Update 1822.cpp

--- 2023-2/Intermediate/dameun2224/BinarySearch/1822.cpp
+++ 2023-2/Intermediate/dameun2224/BinarySearch/1822.cpp
@@ -3,37 +3,42 @@
 #include <algorithm>
 using namespace std;
 
-int n, m, k;
-int a[500005];
+int n, m;
+int a[500005], b[500005];
+vector<int> v;
 
 int bs(int t) {
 	int st = 0;
-	int en = n - 1;
+	int en = m - 1;
 
 	while (st <= en) {
 		int mid = (st + en) / 2;
-		if (a[mid] < t)st = mid + 1;
-		else if (a[mid] > t) en = mid - 1;
-		else return 1;
+		if (b[mid] < t) st = mid + 1;
+		else if (b[mid] > t) en = mid - 1;
+		else return 0;
 	}
 
-	return 0;
+	v.push_back(t);
+	return 1;
 }
 
 int main(void) {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	cin >> n;
-	for (int i = 0; i < n; i++)
-		cin >> a[i];
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) cin >> a[i];
+	for (int i = 0; i < m; i++) cin >> b[i];
 	sort(a, a + n);
-
-	cin >> m;
-	while (m--) {
-		cin >> k;
-		cout << bs(k) << ' ';
+	sort(b, b + m);
+	
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += bs(a[i]);
 	}
+
+	cout << sum << '\n';
+	for (int e : v) cout << e << ' ';
 
 	return 0;
 }
