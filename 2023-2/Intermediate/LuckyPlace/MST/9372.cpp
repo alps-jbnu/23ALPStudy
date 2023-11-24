dc912db67fb3dee36d425a69eeabbcda8a220f3d
남기정 <114844315+LuckyPlace@users.noreply.github.com>
2023-11-24 09:31:21 +0900

    Create 9372.cpp

---
+++ 2023-2/Intermediate/LuckyPlace/MST/9372.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int t, v, e;
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> t;
+	while (t--)
+	{
+		cin >> v >> e;
+		while (e--)
+		{
+			int a, b;
+			cin >> a >> b;
+		}
+		cout << v - 1 << "\n";
+	}
+	return 0;
+}
